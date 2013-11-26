13be4bfdd5c857de91d38d53d4aed4400d4d2893
whr <wilfredo.hr@gmail.com>
2013-11-26 16:28:34 -0500

    win7 001

--- src/com/sblm/bean/Prueba.java
+++ src/com/sblm/bean/Prueba.java
@@ -4,9 +4,44 @@ public class Prueba {
 
 	private int val;
 	private int prim;
-
-	
+	private int sec;
 	private int terc;
+	private int cuarto;
+	private int quinto;
+	private int sexto;
+
+	public int getQuinto() {
+		return quinto;
+	}
+
+	public void setQuinto(int quinto) {
+		this.quinto = quinto;
+	}
+
+	public int getTerc() {
+		return terc;
+	}
+
+	public void setTerc(int terc) {
+		this.terc = terc;
+	}
+
+	public int getCuarto() {
+		return cuarto;
+	}
+
+	public void setCuarto(int cuarto) {
+		this.cuarto = cuarto;
+	}
+
+	public int getSec() {
+		return sec;
+	}
+
+	public void setSec(int sec) {
+		this.sec = sec;
+	}
+
 	public int getVal() {
 		return val;
 	}
