617be508bd6c29abfc1fc3609d7003e7ab8463b0
whr <wilfredo.hr@gmail.com>
2013-11-26 17:23:47 -0500

    cambio 01

--- src/com/sblm/bean/Prueba.java
+++ src/com/sblm/bean/Prueba.java
@@ -2,62 +2,6 @@ package com.sblm.bean;
 
 public class Prueba {
 
-	private int val;
-	private int prim;
-	private int sec;
-	private int terc;
-	private int cuarto;
-	private int quinto;
-	private int sexto;
-	private int set7;
-
-	public int getQuinto() {
-		return quinto;
-	}
-
-	public void setQuinto(int quinto) {
-		this.quinto = quinto;
-	}
-
-	public int getTerc() {
-		return terc;
-	}
-
-	public void setTerc(int terc) {
-		this.terc = terc;
-	}
-
-	public int getCuarto() {
-		return cuarto;
-	}
-
-	public void setCuarto(int cuarto) {
-		this.cuarto = cuarto;
-	}
-
-	public int getSec() {
-		return sec;
-	}
-
-	public void setSec(int sec) {
-		this.sec = sec;
-	}
-
-	public int getVal() {
-		return val;
-	}
-
-	public void setVal(int val) {
-		this.val = val;
-	}
-
-	public int getPrim() {
-		return prim;
-	}
-
-	public void setPrim(int prim) {
-		this.prim = prim;
-	}
-	
+	private int num01;
 	
 }
