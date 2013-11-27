a7246b744842dc3e7f0205091e7bb8cc4bb6787b
whr <wilfredo.hr@gmail.com>
2013-11-27 12:47:47 -0500

    jjk

--- src/com/sblm/bean/Prueba.java
+++ src/com/sblm/bean/Prueba.java
@@ -2,11 +2,6 @@ package com.sblm.bean;
 
 public class Prueba {
 
-private int valor01;
-private int valor02;
 
-
-	
-	
 	
 }
--- src/com/sblm/bean/Prueba2.java
+++ src/com/sblm/bean/Prueba2.java
@@ -1,5 +1,17 @@
 package com.sblm.bean;
 
 public class Prueba2 {
+ int valor01;
+ int valor02;
+ int valor03;
 
+ 
+ 
+int valor07;
+
+ int valor05;
+ int valor06;
+ 
+ int valor08;
+ int valor09;
 }
