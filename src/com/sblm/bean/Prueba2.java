31f049c9de4b56c5c171e0d9113efd828c708698
whr <wilfredo.hr@gmail.com>
2013-11-27 12:42:10 -0500

    kk

--- src/com/sblm/bean/Prueba2.java
+++ src/com/sblm/bean/Prueba2.java
@@ -1,16 +1,5 @@
 package com.sblm.bean;
 
 public class Prueba2 {
- int valor01;
- int valor02;
- int valor03;
- int valor04;
- 
-int valor07;
 
- int valor05;
- int valor06;
- 
- int valor08;
- int valor08;
 }
