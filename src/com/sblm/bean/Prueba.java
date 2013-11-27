a1fff2629f0a9d696188e6a1efa53d7610e65b20
whr <wilfredo.hr@gmail.com>
2013-11-27 10:44:15 -0500

    mi

--- src/com/sblm/bean/Prueba.java
+++ src/com/sblm/bean/Prueba.java
@@ -3,10 +3,6 @@ package com.sblm.bean;
 public class Prueba {
 	private void metodo() {
 		System.out.println("hola mundo nuevo");
-
-	private void metodo() {
-		System.out.println("hola mundo nuevo");
-
 		}
 
 	
