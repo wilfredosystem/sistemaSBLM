b89afcbede8c97e61e524a8889e0b95b7d3fc0a6
Administrador <Administrador@ordenador>
2013-11-27 01:40:16 +0100

    valor01

--- src/com/sblm/bean/Prueba.java
+++ src/com/sblm/bean/Prueba.java
@@ -2,11 +2,6 @@ package com.sblm.bean;
 
 public class Prueba {
 
-<<<<<<< HEAD
-	private int val01;
-	
-=======
-	private int num0111;
->>>>>>> branch 'master' of https://github.com/wilfredosystem/sistemaSBLM.git
+private int valor01;
 	
 }
