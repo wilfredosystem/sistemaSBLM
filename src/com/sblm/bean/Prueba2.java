cd6fcfe1bb4dfe48d14f0761597eb2b718a22797
whr <wilfredo.hr@gmail.com>
2013-11-27 13:08:38 -0500

    num01

--- src/com/sblm/bean/Prueba2.java
+++ src/com/sblm/bean/Prueba2.java
@@ -1,20 +1,7 @@
 package com.sblm.bean;
 
 public class Prueba2 {
- int valor01;
- int valor02;
- int valor03;
-
-
-int valor07;
-
- int valor05;
- int valor06;
- 
- int valor08;
- int valor09;
- 
- int valor10;
+ int numero01;
 
  
 
